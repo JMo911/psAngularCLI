94ea7756076f724d769bcc032adbc7fafcdfa63c
Jacob Moss <jmoss@indeed.com>
2020-04-18 16:46:12 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+const num = 7;
+
 @Component({
   selector: 'jm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app7';
+  title = 'angular cli course';
+
+  sayHello() {
+    const x = 10;
+  }
 }
